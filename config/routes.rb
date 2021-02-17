5391f7b631d6723c08158d2a2c854585d4d320f9
Hena Fufa <swengineer2.whizkids@gmail.com>
2021-02-17 04:26:53 -0800

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   get 'posts/new'
   get 'users/new'
@@ -18,4 +20,6 @@ Rails.application.routes.draw do
   # root 'application#sample'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit,:update]
+    
 end
