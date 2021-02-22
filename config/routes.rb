3faf1037d11af6cfb7e472b514aad9eacddd9cb7
Hena Fufa <swengineer2.whizkids@gmail.com>
2021-02-22 02:51:37 -0800

    folliwing users

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,20 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # get 'static_pages/home'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'application#sample'
-  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit,:update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
     
 end
